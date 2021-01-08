826c99cbf146cf8e356cc3cc54ea298d5b0e8564
Jetniphan <34912803+Jeniphan@users.noreply.github.com>
2021-01-08 13:55:06 +0700

    Add files via upload

---
+++ User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    username: {
+        type: String,
+        unique: true
+    },
+    password: String
+});
+
+const UserModle = mongoose.model('User', userSchema);
+
+module.exports = UserModle;
+
---
+++ db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://34.87.28.105:27017/ahoy-node-passport',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
